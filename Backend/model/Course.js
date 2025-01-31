abd216e1966e478a86125ff2607cda1e69174f08
Devesh Gadhave <deveshgadhave2612@gmail.com>
2025-01-31 23:20:22 +0530

    working on models

--- Backend/model/Course.js
+++ Backend/model/Course.js
@@ -1,1 +1,48 @@
 // 2nd Session lecture 1 1,14,44
+
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+    couresName:{
+        type:String,
+        trim:true,
+    },
+    courseDescription:{
+        type:String,
+
+    },
+    instructor:{
+        type:mongoose.Schema.Types.ObjectId,
+
+    },
+    whatYouWillLearn:{
+        type:String, 
+    },
+    courseContent:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Section",
+        }
+    ],
+    ratingAndRevives:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"RatingAndReview"
+    }],
+    Price:{
+        type:Number,
+    },
+    thumbNail:{
+        type:String,
+    },
+    tag:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Tag",
+    },
+    studentsEnrolled:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }]
+})
+
+
+module.exports=mongoose.model("Course",courseSchema);
---
+++ Backend/model/RatingAndReview.js
@@ -0,0 +1,20 @@
+const mongoose =require('mongoose');
+
+const RatingAndReview =new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    rating:{
+        type:Number,
+        required:true,
+    },
+    review:{
+        type:String,
+        require:true,
+        trim:true,
+    }
+})
+
+module.exports("RatingAndReview",RatingAndReview);
